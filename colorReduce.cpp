2f2db81797fa7a0cc1cc3eb8832b53172375a6c0
bernardndegwa <mrbernardndegwa@gmail.com>
2015-07-04 17:44:36 +0300

    Additional methods for pixel manipulation

--- colorReduce.cpp
+++ colorReduce.cpp
@@ -12,18 +12,23 @@ void colorReduce(cv::Mat image, int div){
 	
 	for (int j=0; j<nl; j++){
 		//get the address of row j
-		uchar* data= image.ptr<uchar>(j);
+		uchar* data= image.ptr<uchar>(j);	
+		//This method image.ptr<TYPE> directly gives you the address of
+		//the image row.
 		
 		for (int i=0; i<nc; i++){
 			//process each pixel
 			data[i]= data[i]/div*div + div/2;
 			//end of each line
+			//an alternative way to move from column to column would:
 			
-			//or use bitwise operations which result in efficeincy 
+			*data = *data/(div*div) + (div/2); data++;
+			
+			//or use bitwise operations which result in efficiency 
 			//especially in embedded processing
 			
-			/*int n;
-			div= pow(2,n);
+			/*int n; we assume that div will be always be a power of 2
+			//div= pow(2,n);
 			uchar mask= 0xFF<<n;	//the mask to round the pixel value
 									//for div=16 , mask= 0xF0
 			*data &=mask;		//masking
@@ -32,6 +37,22 @@ void colorReduce(cv::Mat image, int div){
 		}//end of pixel processing
 		
 	}
+}
+
+//An alternative way to change the image and save it
+//The resulting image should be stored in a data buffer with a size,
+//and pixel type that matches the input type.
+cv::Mat image= cv::imread("/home/bernard/pics/6.jpg"); //read the image
+cv::Mat result;
+result.create(image.rows, image.cols, image.type());
+//The	allocated	memory	block	has	a	size	of	total()*elemSize().	
+//The	looping	is	then	done with	two	pointers:
+//
+
+void colorReduce(const::cv Mat &image, cv::Mat &result, int div=64)
+{
+//with this method signature the image is passed in as a const and will not 
+//be changed while the address of the result is where the image.
 }
 
 int main(int argc, char** argv){
